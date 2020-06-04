59421153577858977a7278309ab2e63c4895593c
zain1326 <ranazain1326@gmail.com>
2020-06-05 02:15:45 +0500

    working on home page

--- public/app.js
+++ public/app.js
@@ -2,15 +2,25 @@
 setTimeout(() => {
     var a =  document.getElementById('form-show');
     a.classList.add("show");
-    body.classList.add("bb");
 }, 1000);
 
 function hide(){
   var a =  document.getElementById('form-show');
- a.classList.add("hide")
- body.classList.remove("bb");
- 
+ a.classList.add("hide"); 
 }
+function hides(){
+    var b =  document.getElementById('search-b');  
+   b.classList.add("hide") 
+ b.classList.remove("show");
+  }
+
+  function shows(){
+    var b =  document.getElementById('search-b');  
+   b.classList.add("show") 
+ b.classList.remove("hide");
+   
+  }
+
 function problem(){
     alert('Please Login First');
 }
